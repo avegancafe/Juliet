05b60db1698c5836499bbca4129934da1b8ecad3
kyle <kylelholzinger@gmail.com>
2021-09-29 15:50:52 -0400

    [KH] remove extraneous config

--- lua/initializers/lspconfig.lua
+++ lua/initializers/lspconfig.lua
@@ -20,7 +20,6 @@ local on_attach = function(client, bufnr)
   local opts = { noremap=true, silent=true }
 
   -- See `:help vim.lsp.*` for documentation on any of the below functions
-  print('setting up keybindings for lspconfig')
   buf_set_keymap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
   buf_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
   buf_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
