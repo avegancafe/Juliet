d046b1c1a490249e48f52255a93cb754c62c1efd
Kyle Holzinger <kyle@relaypayments.com>
2021-12-09 00:31:40 -0500

    [KH] if in git repo, show git status in dashboard

--- nvim/init.lua
+++ nvim/init.lua
@@ -36,16 +36,37 @@ vim.g.dashboard_custom_header = {
   '     │  │   '
 }
 
-local socket = io.popen("fortune")
-local fortune = socket:read("*a")
-socket:close()
+local utils = require('telescope.utils')
+local set_var = vim.api.nvim_set_var
 
-local footer = {}
-for s in fortune:gmatch("[^\r\n]+") do
-  table.insert(footer, s)
+local git_root, ret = utils.get_os_command_output({ "git", "rev-parse", "--show-toplevel" }, vim.loop.cwd())
+
+local function get_dashboard_git_status()
+  local git_cmd = {'git', 'status', '-s', '--', '.'}
+  local output = utils.get_os_command_output(git_cmd)
+  set_var('dashboard_custom_footer', {'Git status', '', unpack(output)})
 end
 
-vim.g.dashboard_custom_footer = footer
+if ret ~= 0 then
+  local is_worktree = utils.get_os_command_output({ "git", "rev-parse", "--is-inside-work-tree" }, vim.loop.cwd())
+  if is_worktree[1] == "true" then
+    get_dashboard_git_status()
+  else
+    local socket = io.popen("fortune")
+    local fortune = socket:read("*a")
+    socket:close()
+
+    local footer = {}
+    for s in fortune:gmatch("[^\r\n]+") do
+      table.insert(footer, s)
+    end
+
+    set_var('dashboard_custom_footer', footer)
+  end
+else
+    get_dashboard_git_status()
+end
+
 
 vim.g.dashboard_custom_shortcut = {
   last_session = ' \\ s l',
