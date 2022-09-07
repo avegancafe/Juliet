3984f5b881b8975855a155428b7abcacab91e696
Kyle Holzinger <kyle@relaypayments.com>
2022-09-07 15:05:22 -0400

    [KH] remove broken mappings

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -446,11 +446,9 @@ return require("packer").startup({
 						documentation = cmp.config.window.bordered(),
 					},
 					mapping = cmp.mapping.preset.insert({
-						["<C-b>"] = cmp.mapping.scroll_docs(-4),
-						["<C-f>"] = cmp.mapping.scroll_docs(4),
-						["<C-Space>"] = cmp.mapping.complete(),
 						["<C-e>"] = cmp.mapping.abort(),
-						["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+						["<CR>"] = cmp.mapping.confirm({ select = true }),
+						["<Tab>"] = cmp.mapping.confirm({ select = true }),
 					}),
 					sources = cmp.config.sources({
 						{ name = "nvim_lsp" },
