4e4ba6a3354c9319a0f6f2c98a8bbc5cebe201d7
Kyle Holzinger <kylelholzinger@gmail.com>
2022-10-19 14:35:06 -1000

    [KH] source .env from fish

--- fish/config.fish
+++ fish/config.fish
@@ -25,6 +25,14 @@ set -gx TERM xterm-256color
 
 source ~/.config/Juliet/fish/_util.fish
 
+function envsource
+  for line in (cat $argv | grep -v '^#' | grep -v '^\s*$')
+    set item (string split -m 1 '=' $line)
+    set -gx $item[1] $item[2]
+    echo "Exported key $item[1]"
+  end
+end
+
 function p
     pnpm $argv
 end
