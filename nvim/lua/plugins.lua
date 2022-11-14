20593405c75838bf229b1bcf75cae3a6f9ec2fb2
Kyle Holzinger <kyle@relaypayments.com>
2022-11-14 14:20:03 -0500

    [KH] add noice.nvim

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -275,8 +275,10 @@ return require('packer').startup({
 				end
 
 				if ret ~= 0 then
-					local is_worktree =
-						utils.get_os_command_output({ 'git', 'rev-parse', '--is-inside-work-tree' }, vim.loop.cwd())
+					local is_worktree = utils.get_os_command_output(
+						{ 'git', 'rev-parse', '--is-inside-work-tree' },
+						vim.loop.cwd()
+					)
 					if is_worktree[1] == 'true' then
 						get_dashboard_git_status()
 					else
@@ -469,14 +471,82 @@ return require('packer').startup({
 						['<Tab>'] = cmp.mapping.confirm({ select = true }),
 					}),
 					sources = cmp.config.sources(
-						{ { name = 'nvim_lsp' }, { name = 'luasnip' } },
-						{ { name = 'buffer' } }
+						{ { name = 'nvim_lsp' }, { name = 'luasnip' } }
 					),
 				})
 			end,
 		})
 		use('vimpostor/vim-tpipeline')
 		use('dstein64/vim-startuptime')
+
+		use({
+			'folke/noice.nvim',
+			config = function()
+				require('noice').setup({
+					messages = {
+						view_search = false
+					},
+					cmdline = {
+						format = {
+							search_down = {
+								view = 'cmdline',
+							},
+							search_up = {
+								view = 'cmdline',
+							},
+						},
+					},
+					routes = {
+						{
+							filter = {
+								find = 'No information available',
+							},
+							opts = { skip = true },
+						},
+						{
+							filter = {
+								kind = '',
+							},
+							opts = { skip = true },
+						},
+						{
+							filter = {
+								event = 'msg_show',
+								kind = '',
+								find = 'written',
+							},
+							opts = { skip = true },
+						},
+					},
+					lsp = {
+						-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+						override = {
+							['vim.lsp.util.convert_input_to_markdown_lines'] = true,
+							['vim.lsp.util.stylize_markdown'] = true,
+							['cmp.entry.get_documentation'] = true,
+						},
+					},
+					-- you can enable a preset for easier configuration
+					presets = {
+						bottom_search = true, -- use a classic bottom cmdline for search
+						command_palette = true, -- position the cmdline and popupmenu together
+						long_message_to_split = true, -- long messages will be sent to a split
+						inc_rename = false, -- enables an input dialog for inc-rename.nvim
+						lsp_doc_border = false, -- add a border to hover docs and signature help
+					},
+				})
+				require('telescope').load_extension('noice')
+			end,
+			requires = {
+				'nvim-telescope/telescope.nvim',
+				-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+				'MunifTanjim/nui.nvim',
+				-- OPTIONAL:
+				--   `nvim-notify` is only needed, if you want to use the notification view.
+				--   If not available, we use `mini` as the fallback
+				'rcarriga/nvim-notify',
+			},
+		})
 	end,
 	auto_reload_compiled = true,
 	config = {
