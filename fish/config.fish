f4e60ac40c6e06317f3fe7f6315fc4422140ff5e
Kyle Holzinger <kyle@relaypayments.com>
2023-06-28 20:03:00 -0400

    [KH] fix chagned_files func to handle new files

--- fish/config.fish
+++ fish/config.fish
@@ -262,7 +262,7 @@ function glo --description "open the URL of a file in gitlab"
 end
 
 function changed_files
-    echo (git status --short | sed "s/[[:alnum:]]\{1,2\}[[:space:]]\(.*\)/\1/g")
+    echo (git status --short | sed "s/[[:alnum:]?]\{1,2\}[[:space:]]\(.*\)/\1/g")
 end
 
 function rtb
