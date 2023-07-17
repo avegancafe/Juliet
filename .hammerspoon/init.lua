adedbb334a0ed4addb5d3196f55a9b3943d32701
Kyle Holzinger <kyle@relaypayments.com>
2023-07-17 12:09:06 -0400

    [KH] format hammerspoon init

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,14 +1,14 @@
-hs.hotkey.bind({"ctrl"}, "space", function()
-    local app = hs.application.get("kitty")
-    if app then
-        if not app:mainWindow() then
-            app:selectMenuItem({"kitty", "New OS window"})
-        elseif app:isFrontmost() then
-            app:hide()
-        else
-            app:activate()
-        end
-    else
-        hs.application.launchOrFocus("kitty")
-    end
+hs.hotkey.bind({ 'ctrl' }, 'space', function()
+	local app = hs.application.get('kitty')
+	if app then
+		if not app:mainWindow() then
+			app:selectMenuItem({ 'kitty', 'New OS window' })
+		elseif app:isFrontmost() then
+			app:hide()
+		else
+			app:activate()
+		end
+	else
+		hs.application.launchOrFocus('kitty')
+	end
 end)
