4b436104fd7a4c562051f112d1c899afe75f28d2
Kyle Holzinger <kyle@relaypayments.com>
2023-11-28 12:52:21 -0500

    [KH] add helper to cd around to different dirs

--- fish/config.fish
+++ fish/config.fish
@@ -319,9 +319,61 @@ function rtb
 end
 
 function note
-    echo "date: $(date)" >>$HOME/notes.txt
-    echo "$argv" >>$HOME/notes.txt
-    echo "" >>$HOME/notes.txt
+    echo "date: $(date)" >> $HOME/notes.txt
+    echo "$argv" >> $HOME/notes.txt
+    echo "" >> $HOME/notes.txt
+end
+
+function work
+    if ! test -e ~/.workspaces && ! test -e ~/.workspaces__local
+        echo "Looks like you don't have workspaces set up. To use this function, create a
+file called ~/.workspaces or ~/.workspaces__local that looks like the following:
+
+```
+@workspace ~/workspace
+@dir ~/.config/nvim
+```
+where @workspace denotes a directory in which many other directories reside, 
+and @dir denotes a specific directory that should be listed when running this
+command"
+        return 1
+    end
+    set -l all_workspaces ""
+
+    if test -e ~/.workspaces
+        set all_workspaces "$(cat ~/.workspaces)"
+    end
+
+    if test -e ~/.workspaces__local
+        set all_workspaces "$all_workspaces
+$(cat ~/.workspaces__local)"
+    end
+
+    set -l dirs (echo "$all_workspaces" | grep @dir | awk '{print $2}')
+    set -l workspaces (echo "$all_workspaces" | grep @workspace | awk '{print $2}')
+
+    set -l fin ""
+
+    if test "$dirs" != ""
+        set fin "$dirs"
+    end
+
+    for dir in (string split '\n' "$workspaces");
+        if test -n "$dir"
+            set fin "$fin
+$(ls -1d $dir/*/)"
+        end
+    end
+
+    set -l selection (echo "$fin" | gum filter --limit 1 --height 10 --placeholder "Which directory would you like to cd to?")
+
+    if test -z "$selection"
+        log Exiting...
+        return 1
+    end
+
+    log "cd-ing to $selection..."
+    cd $selection
 end
 
 eval (starship init fish)
