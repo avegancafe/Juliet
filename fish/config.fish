e8820b065a0212413f190b085a67e5921a40b763
Kyle Holzinger <kyleholzinger@hey.com>
2024-01-10 10:24:31 -0500

    [KH] add support for venv

--- fish/config.fish
+++ fish/config.fish
@@ -398,3 +398,30 @@ set --export PATH $BUN_INSTALL/bin $PATH
 
 # glow
 glow completion fish | source
+
+function __auto_source_venv --on-variable PWD --description "Activate/Deactivate virtualenv on directory change"
+  status --is-command-substitution; and return
+
+  # Check if we are inside a git directory
+  if git rev-parse --show-toplevel &>/dev/null
+    set gitdir (realpath (git rev-parse --show-toplevel))
+    set cwd (pwd -P)
+    # While we are still inside the git directory, find the closest
+    # virtualenv starting from the current directory.
+    while string match "$gitdir*" "$cwd" &>/dev/null
+      test -e $cwd/.venv/bin/activate.fish
+      if test -e "$cwd/venv/bin/activate.fish"
+        source "$cwd/venv/bin/activate.fish" &>/dev/null 
+        return
+      else
+        set cwd (path dirname "$cwd")
+      end
+    end
+  end
+  # If virtualenv activated but we are not in a git directory, deactivate.
+  if test -n "$VIRTUAL_ENV"
+    deactivate
+  end
+end
+
+__auto_source_venv
