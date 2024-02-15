f18bc7a962f281840a0c9036f76fbf2aae82f439
Kyle Holzinger <kyleholzinger@hey.com>
2024-02-15 13:26:47 -0500

    [KH] whoops I removed hammerspoon

---
+++ symlinked/home/.hammerspoon/init.lua
@@ -0,0 +1,21 @@
+open_terminal = function()
+	local app = hs.application.get('kitty')
+	if app then
+		if not app:mainWindow() then
+			app:selectMenuItem({ 'kitty', 'New OS window' })
+		elseif app:isFrontmost() then
+			app:hide()
+		else
+			app:activate()
+		end
+	else
+		hs.application.launchOrFocus('kitty')
+	end
+end
+
+hs.hotkey.bind({}, 'f14', open_terminal)
+hs.hotkey.bind({ 'ctrl' }, 'space', open_terminal)
+
+hs.hotkey.bind({}, 'f15', function()
+	hs.caffeinate.lockScreen()
+end)
