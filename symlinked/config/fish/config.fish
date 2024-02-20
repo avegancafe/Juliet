b31aa1ef94e19efbbf4e0f6f275bb63ebe815b76
Kyle Holzinger <kyleholzinger@hey.com>
2024-02-20 11:23:01 -0500

    feat(gcloud): add function to tail logs from gcloud services

--- symlinked/config/fish/config.fish
+++ symlinked/config/fish/config.fish
@@ -444,3 +444,9 @@ __auto_source_venv
 if command -v zoxide 2&> /dev/null
     zoxide init fish --cmd cd | source
 end
+
+function logs
+    set -l service (echo '["j2-network-manger", "network-intel-dashboard", "network-intel-dashboard-qa"]' | jq -r .[] | fzf --reverse --height 40% --prompt "Which service logs would you like to see? ")
+
+    gcloud app logs tail --service $service
+end
