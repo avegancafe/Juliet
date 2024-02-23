54c3989e9e611b8be0c7fe13ecff149cbfb10588
Kyle Holzinger <kyleholzinger@hey.com>
2024-02-23 16:30:35 -0500

    [KH] add staging as an option for log tail

--- symlinked/config/fish/config.fish
+++ symlinked/config/fish/config.fish
@@ -446,7 +446,7 @@ if command -v zoxide 2&> /dev/null
 end
 
 function logs
-    set -l service (echo '["j2-network-manger", "network-intel-dashboard", "network-intel-dashboard-qa"]' | jq -r .[] | fzf --reverse --height 40% --prompt "Which service logs would you like to see? ")
+    set -l service (echo '["j2-network-manger", "network-intel-dashboard", "network-intel-dashboard-qa", "network-intel-dashboard-staging"]' | jq -r .[] | fzf --reverse --height 40% --prompt "Which service logs would you like to see? ")
 
     gcloud app logs tail --service $service
 end
