fed57204cba99a6391244adf47a6e63a50024816
Kyle Holzinger <kyleholzinger@hey.com>
2024-03-06 10:08:40 -0500

    [KH] make key on keyboard open/close neovide

--- symlinked/home/.hammerspoon/init.lua
+++ symlinked/home/.hammerspoon/init.lua
@@ -1,4 +1,4 @@
-open_terminal = function()
+local open_terminal = function()
 	local app = hs.application.get('kitty')
 	if app then
 		if not app:mainWindow() then
@@ -13,7 +13,23 @@ open_terminal = function()
 	end
 end
 
-hs.hotkey.bind({}, 'f14', open_terminal)
+local open_neovide = function()
+	local app = hs.application.get('Neovide')
+	if app then
+		if not app:mainWindow() then
+			app:quit()
+			hs.application.launchOrFocus('/opt/homebrew/Cellar/neovide/0.12.2/Neovide.app')
+		elseif app:isFrontmost() then
+			app:hide()
+		else
+			app:activate()
+		end
+	else
+		hs.application.launchOrFocus('/opt/homebrew/Cellar/neovide/0.12.2/Neovide.app')
+	end
+end
+
+hs.hotkey.bind({}, 'f14', open_neovide)
 hs.hotkey.bind({ 'ctrl' }, 'space', open_terminal)
 
 hs.hotkey.bind({}, 'f15', function()
