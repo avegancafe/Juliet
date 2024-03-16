3b7942d0c681db0c097844dae9bdb3e977dcd8ca
Kyle Holzinger <kyleholzinger@hey.com>
2024-03-16 01:36:06 -0400

    [KH] install cask instaed of formula

--- symlinked/config/fish/config.fish
+++ symlinked/config/fish/config.fish
@@ -213,7 +213,7 @@ end
 
 function nv --wraps "neovide"
     if ! command -v neovide 2&> /dev/null
-        gum confirm "Neovide not installed. Install?" && brew install neovide
+        gum confirm "Neovide not installed. Install?" && brew install --cask neovide
     end
 
     neovide $argv
