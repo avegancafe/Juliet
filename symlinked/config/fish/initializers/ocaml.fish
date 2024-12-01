daa9aa93dbfb4c99a2574f4256567d6b017397ec
Kyle Holzinger <kyleholzinger@hey.com>
2024-12-01 17:39:35 -0500

    [KH] add ocaml support (might break but I think opam is in the brewfile)

---
+++ symlinked/config/fish/initializers/ocaml.fish
@@ -0,0 +1,7 @@
+# BEGIN opam configuration
+# This is useful if you're using opam as it adds:
+#   - the correct directories to the PATH
+#   - auto-completion for the opam binary
+# This section can be safely removed at any time if needed.
+# test -r '/Users/kyle/.opam/opam-init/init.fish' && source '/Users/kyle/.opam/opam-init/init.fish' > /dev/null 2> /dev/null; or true
+# END opam configuration
